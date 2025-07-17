6eb76bf34ea61391d20aa39546cd0898f9852fa8
Matteo Rigamonti <matteo_rigamonti@hotmail.it>
2025-07-17 14:42:20 +0200

    chore: add @astrojs/netlify dependency to package.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,17 @@
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
   devToolbar: {
     enabled: false,
   },
+
   vite: {
     plugins: [tailwindcss()],
   },
+
+  adapter: netlify(),
 });
